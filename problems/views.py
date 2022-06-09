2182958b59bcce72520684b160b4826e9a97c98f
Bart Dorsey <bart@bartdorsey.com>
2022-06-09 16:49:28 -0500

    Adding some comments

--- problems/views.py
+++ problems/views.py
@@ -3,6 +3,7 @@ from problems.models import Person
 from django.contrib.auth.models import User
 
 from prettyprinter import cpprint, install_extras
+
 install_extras()
 
 # problem lists
@@ -11,23 +12,26 @@ names_problems = list(range(5, 12)) + list(range(19, 23))
 people_problems = list(range(23, 42))
 user_problems = [12]
 
+# /problems
 def show_problem_list(request):
-    context = {
-        "problems": range(1, 42)
-    }
-    return render(request, 'problem_list.html', context)
+    """View for showing the list of problems"""
+    context = {"problems": range(1, 42)}
+    return render(request, "problem_list.html", context)
 
+
+# /problems/<int:problem_number>
 def show_problem(request, problem_number):
+    """View for displaying a specific problem"""
     print(f"Trying to load problem {problem_number}")
     template_name = f"problem_{str(problem_number).zfill(3)}.html"
 
     context = {}
 
-    # Problem 4
+    # Context for Problem 4
     if problem_number in show_information_problems:
         context["show_information"] = False
 
-    # Problems 5 through 11 and 19 through 22
+    # Context for Problems 5 through 11 and 19 through 22
     if problem_number in names_problems:
         context["names"] = [
             "Kim",
@@ -36,16 +40,16 @@ def show_problem(request, problem_number):
             "Anima",
         ]
 
-    # Problem 12
+    # Context for Problem 12
     if problem_number in user_problems:
         context["user"]: User.objects.first()
 
-    # Problems 23 through 41
+    # Context for Problems 23 through 41
     if problem_number in people_problems:
         context["people"] = Person.objects.all()
 
     # This prints out the context in a pretty way.
-    print("="*10, "Context", "="*10)
+    print("=" * 10, "Context", "=" * 10)
     cpprint(context)
 
     return render(request, f"problems/{template_name}", context)
